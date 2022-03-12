aa645abfd15ea0b8a5395c78d79c101fd2bf297c
= <3lian3.pereira@gmail.com>
2022-03-12 14:34:25 -0300

    Fix Anchor href

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -8,14 +8,15 @@ function Navbar() {
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
                     <a href="https://github.com/eliane-sp-silva" target="_blank"
-                        rel="noreferrer" />
-                    <div className="dsmovie-contact-container">
-                        <GithubIcon />
-                        <p className='dsmovie-contact-link'>/devsuperior</p>
-                    </div>
+                        rel="noreferrer">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className='dsmovie-contact-link'>/eliane-sp-silva</p>
+                        </div>
+                    </a>
                 </div>
             </nav>
-        </header>
+        </header >
     );
 }
 export default Navbar;
