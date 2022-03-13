f02b9e7d2f363097da423cdfdf91794421fde521
= <3lian3.pereira@gmail.com>
2022-03-13 14:16:05 -0300

    Show score

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -2,17 +2,13 @@ import FormCard from 'components/FormCard';
 import { useParams } from 'react-router-dom';
 
 
-
-
 function Form() {
 
     const params = useParams();
 
     return (
 
-        <FormCard movieId={`${params.movieId}`} />
-
-       
+        <FormCard movieId={`${params.movieId}`} />      
        
 
     );
